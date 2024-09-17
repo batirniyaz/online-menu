047356b480aa1cb04b66e7bc3ecb09fcf72e63b4
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-17 09:38:54 +0500

    configure sub category db and pydantic models

---
+++ app/models/sub_category.py
@@ -0,0 +1,25 @@
+import datetime
+from typing import List
+
+from sqlalchemy import Integer, String, ForeignKey, TIMESTAMP
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from app.auth.database import Base
+from app.models.product import Product
+
+
+class SubCategory(Base):
+    __tablename__ = "sub_category"
+
+    id: Mapped[int]= mapped_column(Integer, unique=True, index=True, nullable=False, primary_key=True)
+    name: Mapped[str] = mapped_column(String(length=255), nullable=False)
+    sort_order: Mapped[int] = mapped_column(Integer, nullable=False, index=True)
+    category_id: Mapped[int] = mapped_column(ForeignKey("category.id"), nullable=False)
+    products: Mapped[List["Product"]] = relationship(back_populates="sub_category", lazy="selectin")
+
+    category = relationship("Category", back_populates="sub_categories")
+
+    created_at: Mapped[datetime.datetime] = mapped_column(TIMESTAMP(timezone=True),
+                                                          default=lambda: datetime.datetime.now(datetime.timezone.utc))
+    updated_at: Mapped[datetime.datetime] = mapped_column(TIMESTAMP(timezone=True),
+                                                          default=lambda: datetime.datetime.now(datetime.timezone.utc),
+                                                          onupdate=lambda: datetime.datetime.now(datetime.timezone.utc))
---
+++ app/schemas/sub_category.py
@@ -0,0 +1,45 @@
+import datetime
+
+from pydantic import BaseModel, Field
+from typing import Optional
+
+from app.schemas.product import ProductResponse
+
+
+class SubCategoryBase(BaseModel):
+    name: str = Field(..., description="The name of the sub category")
+    sort_order: int = Field(..., description="The sort order of the sub category")
+    category_id: int = Field(..., description="The ID of the category the sub category is associated with")
+
+
+class SubCategoryCreate(SubCategoryBase):
+    pass
+
+
+class SubCategoryUpdate(SubCategoryBase):
+    name: Optional[str] = Field(None, description="The name of the sub category")
+    sort_order: Optional[int] = Field(None, description="The sort order of the sub category")
+    category_id: Optional[int] = Field(None, description="The ID of the category the sub category is associated with")
+
+
+class SubCategoryResponse(SubCategoryBase):
+    id: int = Field(..., description="The ID of the sub category")
+    products: Optional[list[ProductResponse]] = Field([],
+                                                      description="A list of products associated with the sub category")
+
+    created_at: datetime.datetime = Field(..., description="The time the sub category was created")
+    updated_at: datetime.datetime = Field(..., description="The time the sub category was updated")
+
+    class Config:
+        from_attributes = True
+        validate_assignment = True
+        arbitrary_types_allowed = True
+        json_schema_extra = {
+            "example": {
+                "id": 1,
+                "name": "First meal",
+                "sort_order": 1,
+                "category_id": 1,
+                "products": ["Fruit", "Egg"]
+            }
+        }
