381e8369ea65268482e2bb8f9594bc298a5de215
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-17 13:06:53 +0500

    add new version and change uuid field

--- app/models/order.py
+++ app/models/order.py
@@ -1,8 +1,9 @@
 import datetime
 from typing import List, Dict
-from uuid import uuid4 as uuid
+import uuid
 
-from sqlalchemy import Integer, TIMESTAMP, JSON, UUID
+from sqlalchemy import Integer, TIMESTAMP, JSON, Column
+from sqlalchemy.dialects.postgresql import UUID as PG_UUID
 from sqlalchemy.orm import Mapped, mapped_column
 from app.auth.database import Base
 
@@ -11,11 +12,11 @@ class Order(Base):
     __tablename__ = "order"
 
     id: Mapped[int] = mapped_column(Integer, unique=True, index=True, nullable=False, primary_key=True)
-    uuid: Mapped[uuid] = mapped_column(UUID(as_uuid=True), default=uuid, nullable=False)
+    uuid = Column(PG_UUID(as_uuid=True), default=uuid.uuid4, nullable=False)
     products: Mapped[List[Dict]] = mapped_column(JSON, default=[])
 
     created_at: Mapped[datetime.datetime] = mapped_column(TIMESTAMP(timezone=True),
-                                                            default=lambda: datetime.datetime.now(datetime.timezone.utc))
+                                                          default=lambda: datetime.datetime.now(datetime.timezone.utc))
     updated_at: Mapped[datetime.datetime] = mapped_column(TIMESTAMP(timezone=True),
                                                             default=lambda: datetime.datetime.now(datetime.timezone.utc),
                                                             onupdate=lambda: datetime.datetime.now(datetime.timezone.utc))
